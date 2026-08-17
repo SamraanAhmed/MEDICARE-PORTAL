37eef9c1c33fff83e806d09be424945d08bedcf1
Muhammad Hazza Shabir <mhazzashabir.com>
2026-08-17 17:35:59 +0500

    initailizig endpoints file

--- backend/api/data/index.js
+++ backend/api/data/index.js
@@ -1,6 +1,17 @@
+const express = require('express');
 const {
-
+    loginDoctor, loginUser, createDoctor, createUser,
+    getdoctor,
+    createUser, createDoctor, createAppointment, createMessage, createService,
+    updateService, updateAppointment, updateDoctorAvailability,
+    checkDoctorDailyAvailability
 } = require('../../database/queries');
 const { 
     authenticate, checkAuthentication
 } = require('../../authentication');
+
+const router = express.Router();
+
+
+
+module.exports = router;
